9fd634d4265f2977b00cece6cd8bdf87056b753a
Brayan-BH <brayangithub007@gmail.com>
2023-01-22 20:04:25 -0500

    Login

--- components/FormLogin.vue
+++ components/FormLogin.vue
@@ -1,32 +1,39 @@
 <template>
     <main>
         <div class="container-login">
-            <div class="form-container">
-                <div class="head">
-                    <h2>Iniciar Sesión</h2>
-                    <img class="logo__img" src="/img/logo.png" alt="logo">
-                </div>
-                <form>
-                    <div class="body">
-                        <div class="inputbox">
-                            <input type="mail" required>
-                            <span>Coreo Electronico</span>
-                        </div>
-                        <div class="inputbox">
-                            <input type="password" required>
-                            <span>Contraseña</span>
-                        </div>
+            <div class="center-login">
+                <!-- <img class="logo__img" src="/img/logo.png" alt="logo"> -->
+                <h2>Iniciar Sesión</h2>
+                <hr class="divider">
+                <!-- start form -->
+                <div class="form-body-login">
+                    <div class="form-content">
+                        <form>
+                            <!-- Email address input-->
+                            <div class="floating">
+                                <input class="control" name="email" type="email" placeholder="Correo">
+                                <label for="email">Correo</label>
+                            </div>
+                            <!-- Password -->
+                            <div class="floating">
+                                <input class="control" name="password" type="password" placeholder="password">
+                                <label for="password">Contraseñá</label>
+                            </div>
+                            <NuxtLink to="#">¿Has olvidado tu contraseña?</NuxtLink>
+                            <div class="d-grid">
+                                <button class="btn-register">
+                                    <NuxtLink to="/signup">Crear Cuenta
+                                    </NuxtLink>
+                                </button>
+                                <button class="btn-ingresar">
+                                    Ingresar
+                                </button>
+                            </div>
+
+                        </form>
                     </div>
-                </form>
-                <nuxt-link to="#">¿Has olvidado tu correo electrónico?</nuxt-link>
-            </div>
-            <div class="d-grid">
-                <button class="btn-register" id="button-enviar">
-                    <nuxt-link to="/signup">Crear Cuenta</nuxt-link>
-                </button>
-                <button class="btn-ingresar" id="button-enviar">
-                    Ingresar
-                </button>
+                </div>
+                <!-- end form -->
             </div>
         </div>
     </main>
