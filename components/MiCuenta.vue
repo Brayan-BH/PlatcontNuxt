567146224c09fa658cd5173768b83907bfe6f879
Deybin Yoni Gil Perez <deybin_04@hotmail.com>
2023-01-25 11:01:14 -0500

    section mi cuenta

--- components/MiCuenta.vue
+++ components/MiCuenta.vue
@@ -1,20 +1,126 @@
 <template>
-    <div class="container-information">
-        <div class="info-1">
-            <div class="container-card">
-                <div class="header-card">
+    <div class="cuenta">
+        <div class="container-information">
+
+            <div class="info-1">
+                <div class="container-card">
+                    <div class="header-card">
+                        <p class="header-title before">
+                            <button class="title-btn" type="button" role="button" @click="dropClick(1)">
+                                Su Información
+                                <i class="material-icons">
+                                    person
+                                </i>
+                            </button>
+                        </p>
+                    </div>
                     <div class="body-card">
-                        <h1>Basurto Huamán Brayan</h1>
-                        <p>Ad nulla eu aute sit dolor nostrud ad occaecat esse cupidatat.</p>
+                        <div class="body-content">
+                            <div :class="[(interaction.drop == 1) ? 'drop-show' : 'drop-hide']">
+                                <p class="main-title">Basurto Huamán Brayan</p>
+                                <p>Av. Ocopilla 660
+                                </p>
+                                <p>huancayo, junin, 1800</p>
+                                <p>Perú</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="footer-card">
+                        <button class="btn-actualizar">Actualizar</button>
                     </div>
                 </div>
-                <div class="footer-card">
-                    <button>Actualizar</button>
+            </div>
+            <div class="info-2">
+                <div class="container-card">
+                    <div class="header-card">
+                        <p class="header-title before">
+                            <button class="title-btn" type="button" role="button" @click="dropClick(2)">
+                                Su Información
+                                <i class="material-icons">
+                                    person
+                                </i>
+                            </button>
+                        </p>
+                    </div>
+                    <div class="body-card">
+                        <div class="body-content">
+                            <div :class="[(interaction.drop == 2) ? 'drop-show' : 'drop-hide']">
+                                <p class="main-title">Basurto Huamán Brayan</p>
+                                <p>Av. Ocopilla 660
+                                </p>
+                                <p>huancayo, junin, 1800</p>
+                                <p>Perú</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="footer-card">
+                        <button class="btn-actualizar">Actualizar</button>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="info-2">
-
+        <!-- Section Dash -->
+        <div class="container-dashboard">
+            <div class="container-cards">
+                <div class="cards-items">
+                    <div class="items">
+                        <p class="number">1</p>
+                        <i class="material-icons">
+                            settings
+                        </i>
+                        <p class="sub">Items</p>
+                    </div>
+                    <div class="items">
+                        <p class="number">2</p>
+                        <i class="material-icons">
+                            settings
+                        </i>
+                        <p class="sub">Items</p>
+                    </div>
+                    <div class="items">
+                        <p class="number">3</p>
+                        <i class="material-icons">
+                            settings
+                        </i>
+                        <p class="sub">Items</p>
+                    </div>
+                    <div class="items column-3">
+                        <p class="number">4</p>
+                        <i class="material-icons">
+                            settings
+                        </i>
+                        <p class="sub">Items</p>
+                    </div>
+                    <!-- <div class="items">
+                        <p class="number">5 </p>
+                        <i class="material-icons">
+                            settings
+                        </i>
+                        <p class="sub">Items</p>
+                    </div> -->
+                </div>
+            </div>
         </div>
     </div>
 </template>
+<script>
+export default {
+    name: "MiCuenta",
+    mounted() {
+
+    },
+    data() {
+        return {
+            interaction: {
+                drop: 0,
+            }
+        }
+    },
+    methods: {
+        dropClick(index) {
+            if (index == this.interaction.drop) this.interaction.drop = 0
+            else this.interaction.drop = index
+        }
+    }
+}
+</script>
